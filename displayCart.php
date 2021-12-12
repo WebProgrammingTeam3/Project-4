da155a802d618a4dc5a0acf7c1b26313e83638de
Ryan Salter <33379627+rsalter2@users.noreply.github.com>
2021-12-12 14:25:59 -0500

    Cart Files
    helper to add items to cart and to checkout

---
+++ displayCart.php
@@ -0,0 +1,90 @@
+<!doctype html>
+
+<html>
+
+<head lang="en">
+	<meta charset="UTF-8">
+<title>Choose a Product</title>
+	<style>
+	table {
+   border: 1px solid black;
+	border-collapse: collapse;
+}
+
+th, td {
+	border: 1px solid black;
+    text-align: center;
+    padding: 8px;
+}
+tr:nth-child(even){background-color: #a0a0a0}
+tr:nth-child(odd){background-color: #ffffff}
+tr:nth-child(1){font-weight: bold;}
+	</style>
+</head>
+
+<body>
+
+<?php
+$servername = "localhost";
+$username = "rsalter2";
+$password = "rsalter2";
+$dbname = "rsalter2";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT Cart.itemid,Item.name,Item.ctype,Item.color,Item.quantity FROM Cart INNER JOIN Item ON Cart.itemid = Item.itemid WHERE Cart.itemid = Item.itemid AND Item.itemid<=10";
+$result = $conn->query($sql);
+echo "<h3>Your Rentals</h3>";
+if ($result->num_rows > 0) {
+     // output data of each row
+	echo "<table><tr><th>Car ID</th><th>Name</th><th>Type</th><th>color</th><th>Quantity</th></tr>";
+     while($row = $result->fetch_assoc()) {
+     	 $itemid=$row["itemid"];
+		 $name=$row["name"];
+		 $ctype=$row["ctype"];
+		 $color=$row["color"];
+		 $quantity=$row["quantity"];
+
+
+         echo "<tr><td>".$itemid."</td><td>".$name."</td><td>".$ctype."</td><td>".$color."</td><td>".$quantity."</td></tr>";
+     }
+	 echo "</table>";
+} else {
+     echo "0 results";
+}
+
+$sql = "SELECT Cart.itemid, Item.name, Item.snum FROM Cart INNER JOIN Item ON Cart.itemid = Item.itemid WHERE Cart.itemid = Item.itemid AND Item.itemid>=11";
+$result = $conn->query($sql);
+echo "<h3>Parking Spaces</h3>";
+if ($result->num_rows > 0) {
+     // output data of each row
+	echo "<table><tr><th>Space ID</th><th>Space Number</th></tr>";
+     while($row = $result->fetch_assoc()) {
+
+
+     	 $itemid=$row["itemid"];
+		 $snum=$row["snum"];
+         echo "<tr><td>".$itemid."</td></tr><tr><td>".$snum."</td></tr>";
+     }
+	 echo "</table>";
+} else {
+     echo "0 results";
+}
+
+
+$conn->close();
+?>  
+
+	<div>
+	<a href="displayTable.php"><input type="button" id="btn1" value="Back To Inventory"></a>
+	<a href="addAlbum.php"><input type="button" id="btn1" value="Checkout"></a>
+	<a href="login.php"><input type="button" id="btn1" onclick="addA.php" value="Logout"></a>
+	</div>
+</body>
+
+</html>
